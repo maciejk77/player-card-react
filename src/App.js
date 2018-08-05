be6285eb02d38b53bb931a28f93344611ffe55e2
Maciej Kurek <maciejk77@gmail.com>
2018-08-05 23:59:21 +0100

    app completed, but chevron down and club badge positioning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import DropDown from './components/dropdown';
-import PlayerStats from './components/player_stats';
 import { players } from './data/data.json';
+import './app.css';
 
 class App extends Component {
-
-    // this.props.data.filter(pl => (pl.player.id === 4246))
-    //const data = this.players.filter(player => player.player.name.last === name)
-
   render() {
-
     return (
-      <div>
+      <div className="app">
         <DropDown data={players} />
-        <PlayerStats data={players[0]}/>
       </div>
     );
   }
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
+import PlayerStats from './player_stats';
+import '../styles/dropdown.css';
 
 class DropDown extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      current_player: ''
+      current_player: 'Alderweireld'
     }  
     this.handle_change = this.handle_change.bind(this)
   }
   
+  // setting current active player
   handle_change(event) {
     this.setState({ current_player: event.target.value })
   }
 
+  // helper to get a list of player names in dropdown
   render_players() {
-    const players = this.props.data.map(item => item.player.name.last)
+    const players = this.props.data.map(item => item.player.name)
     return players.map(player => 
-      <option key={player} value={player}>{player}</option>
+      <option key={player.last} value={player.last}>{player.first} {player.last}</option>
     )
+  }
+
+  // helper to get data per given name
+  get_player_data(name) {
+    return this.props.data.filter(player => player.player.name.last === name)
   }
 
   render() {    
     return (
-      <select className="card__select" value={this.state.current_player} onChange={this.handle_change}>
-        {/* <option value="">Select a player...</option> */}
-        {this.render_players()}
-      </select>
+      <div className="dropdown">
+        <select className="dropdown__select" value={this.state.current_player} onChange={this.handle_change}>
+          {this.render_players()}
+        </select>
+        <PlayerStats
+          data={this.get_player_data(this.state.current_player)}
+        />
+      </div>
     )
   }
 }
